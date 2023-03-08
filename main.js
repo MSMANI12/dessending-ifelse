5bdb61ad8fe3a221ee5d62cc0b3d48a1d943b234
MSMANI12 <selvamani12121998@gmail.com>
2023-03-08 10:18:36 +0530

    add dessending-ifelse

---
+++ main.js
@@ -0,0 +1,28 @@
+	
+var a=2,b=8,c=6;
+if((a>b)&&(a>c)){
+	if(b>c){
+		console.log(a,b,c);
+	}
+	else{
+		console.log(a,c,b);
+	}
+}
+	if((b>a)&&(b>c)){
+		if(a>c)
+		{
+			console.log(b,a,c);
+		}
+		else{
+			console.log(b,c,a);
+		}
+	}
+	if((c>a)&&(c>b)){
+		if(b>a){
+			console.log(c,b,a);
+		}
+		else{
+			console.log(c,a,b);
+		}
+	}
+		
